3d98b396e1ec5fcbbd2a4afd496139c141d1e94e
hjLv <y.pearl84@gmail.com>
2021-01-18 23:49:37 +0000

    Add user_id to NoteResource

--- app/Http/Resources/NoteResource.php
+++ app/Http/Resources/NoteResource.php
@@ -16,6 +16,7 @@ class NoteResource extends JsonResource
     {
         //return parent::toArray($request);
         return  [
+	    'user_id' => "user_id : " . $this->user_id,		
             'title' => "title : " . $this->title,
             'note' => "note : " . $this->note,
             'create_at' => $this->created_at->diffForHumans()
