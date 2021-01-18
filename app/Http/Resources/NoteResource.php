44ad41cc4b193a8853bbf3beed8980fb7cf18cb7
hjLv <y.pearl84@gmail.com>
2021-01-18 15:51:59 -0800

    Fix NoteResource return

--- app/Http/Resources/NoteResource.php
+++ app/Http/Resources/NoteResource.php
@@ -16,9 +16,9 @@ class NoteResource extends JsonResource
     {
         //return parent::toArray($request);
         return  [
-	    'user_id' => "user_id : " . $this->user_id,		
-            'title' => "title : " . $this->title,
-            'note' => "note : " . $this->note,
+	    'user_id' => $this->user_id,		
+            'title' => $this->title,
+            'note' => $this->note,
             'create_at' => $this->created_at->diffForHumans()
         ];
     }
